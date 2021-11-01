88e784032399e16430789eeae0547cd6fe19c910
Losharik <tankustvotnycke@yandex.ru>
2021-11-01 22:00:32 +0300

    Скелет БД

--- app/models.py
+++ app/models.py
@@ -1,2 +1,26 @@
+from datetime import datetime
+from enum import unique
+from time import time
+from flask import current_app
+from sqlalchemy import event, DDL
+from app import db
 
+
+class Services(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    name = db.Column(db.String(128))
+    price = db.Column(db.Integer)
+
+
+class Personal(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    photo = db.Column(db.String(1024))
+    name = db.Column(db.String(128))
+    position = db.Column(db.String(64))
+
+
+class Texts(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    title = db.Column(db.String(64))
+    text = db.Column(db.Text)
 
