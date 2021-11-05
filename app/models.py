c7d660a2894ecfd3f512a852f7a29e92ac7cbb46
Losharik <tankustvotnycke@yandex.ru>
2021-11-05 19:09:41 +0300

    БД v0.1

--- app/models.py
+++ app/models.py
@@ -6,22 +6,35 @@ from sqlalchemy import event, DDL
 from app import db
 
 
-class Services(db.Model):
+class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
     name = db.Column(db.String(128))
     price = db.Column(db.Integer)
+    number = db.Column(db.Integer, index=True)  # порядковый номер
     category = db.Column(db.String(64))
+    status = db.Column(db.BOOLEAN())  # on/off
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    name = db.Column(db.String(128))
+    status = db.Column(db.BOOLEAN())  # on/off
 
 
 class Personal(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
     photo = db.Column(db.String(1024))
     name = db.Column(db.String(128))
-    position = db.Column(db.String(64))
+    position = db.Column(db.String(64))  # должность
 
 
-class Texts(db.Model):
+class Text(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
-    title = db.Column(db.String(64))
-    text = db.Column(db.Text)
+    title = db.Column(db.String(64))  # название текста
+    text = db.Column(db.Text)  # содержание
 
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True, unique=True)
+    name = db.Column(db.String(128))
+    text = db.Column(db.Text)
