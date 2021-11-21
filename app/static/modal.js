cc5097631a178500af220537daf823a4f6e5e2ab
Losharik <tankustvotnycke@yandex.ru>
2021-11-21 23:43:59 +0300

    модальное окно fix

--- app/static/modal.js
+++ app/static/modal.js
@@ -2,29 +2,23 @@
 
 (function(){
     'use strict';
-    document.addEventListener('click',EasyTogglerHandler);
-    function EasyTogglerHandler(event){
-        const EY_BTN=event.target.closest('[data-easy-toggle]');
-        if(EY_BTN){
-            event.preventDefault();
-            let ey_target=EY_BTN.getAttribute('data-easy-toggle');
-            let ey_class=EY_BTN.getAttribute('data-easy-class');
-            try{document.querySelector(ey_target).classList.toggle(ey_class)}
-            catch(ey_error){console.warn('EasyToggler.js: Блок '+ey_target+' не существует')}
-        }
-        if(!EY_BTN){
-            let ey_rcoe_block_targets=document.querySelectorAll('[data-easy-rcoe]');
-            Array.from(ey_rcoe_block_targets).forEach.call(ey_rcoe_block_targets,function(ey_rcoe_block_target){
-                let ey_rcoe_block=ey_rcoe_block_target.getAttribute('data-easy-toggle'),ey_rcoe_block_class=ey_rcoe_block_target.getAttribute('data-easy-class');
-                if(!event.targetclosest(ey_rcoe_block)){
-                    try{
-                        document.querySelector(ey_rcoe_block).classList.remove(ey_rcoe_block_class)
-                    }
-                    catch(ey_error){
-                        console.warn('EasyToggler.js : Блок '+ey_rcoe_block+' не существует')
-                    }
-                }
-            })
-        }
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        let modal_win = document.getElementById('navigation_buttons')
+
+        modal_win.addEventListener('click', EasyTogglerHandler);
+    })
+
+    function EasyTogglerHandler(event) {
+
+        let EY_BTN=event.target.closest('[data-easy-toggle]');
+
+        if(!EY_BTN) return;
+
+        event.preventDefault();
+        let ey_target=EY_BTN.getAttribute('data-easy-toggle');
+        let ey_class=EY_BTN.getAttribute('data-easy-class');
+        document.querySelector(ey_target).classList.toggle(ey_class)
     }
 })()
