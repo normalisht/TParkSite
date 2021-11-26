6d83717c5873a61d7c5c20a0499f0921716489d9
Losharik <tankustvotnycke@yandex.ru>
2021-11-26 11:39:21 +0300

    Модальное окно(анимация)

--- app/static/modal_window.js
+++ app/static/modal_window.js
@@ -1,5 +1,9 @@
 (function(){
 
+    let time_animation_modal_window = 500
+    document.documentElement.style
+        .setProperty('--modal_window_time', `${time_animation_modal_window}ms`)
+
     let current_modal_window = {
         status: false,
         modal_window_name: '',
@@ -8,51 +12,63 @@
             this.modal_window_fullname = full_name
             this.modal_window_name = full_name.split('-')[0]
         },
-        change_status() {
+        change_status(modal_window_name) {
+
+            let time = !this.status ? 0: time_animation_modal_window
+            setTimeout( () => {
+                document.querySelector(modal_window_name).classList.toggle('show_modal_window')
+            }, time)
+
+            this.set_modal_window_name(modal_window_name)
+
             this.status = !this.status
 
-            if (!status)
+            this.animation(this.modal_window_fullname)
+
+            if (!this.status)
                 this.modal_window_fullname = this.modal_window_name = null
-        }
+        },
+
+        animation(modal_window_name) {
+            try {
+                if (!modal_window_name) return
+
+                let name = modal_window_name.slice(0)
+                document.querySelector(name).firstElementChild
+                    .classList.toggle('open_modal_window')
+                document.querySelector(name).lastElementChild
+                    .classList.toggle('open_overlay')
+            }
+            catch (e) {
+                console.error('Modal window: animation error')
+            }
+        },
     }
+
+    window.addEventListener('resize', modal_windows_position)
+    window.addEventListener('orientationchange', modal_windows_position)
 
     document.addEventListener('DOMContentLoaded', function() {
 
-        let modal_win = document.getElementById('navigation_buttons')
-        modal_win.addEventListener('click', modal_window_toggle);
+        let modal_window_events = document.getElementById('navigation_buttons')
+        modal_window_events.addEventListener('click', modal_window_toggle);
 
         if (!current_modal_window.status)
             window.location.hash = ''
+
+        modal_windows_position()
     })
-
-    function modal_window_toggle(event) {
-
-        let modal_window_btn=event.target.closest('[data-modal-window-toggle]');
-
-        if(!modal_window_btn) return;
-
-        event.preventDefault();
-        let modal_window_target=modal_window_btn.getAttribute('data-modal-window-toggle');
-
-        document.querySelector(modal_window_target).classList.toggle('show_modal_window')
-        current_modal_window.change_status()
-        current_modal_window.set_modal_window_name(modal_window_target)
-
-        // если модальное окно активно изменяем url, иначе откатываем
-        if (current_modal_window.status)
-            window.location.hash += current_modal_window.modal_window_name
-        else
-            history.back()
-    }
 
     window.addEventListener('hashchange', function (event) {
         // история откатилась, но окно открыто, значит закрываем
         if (window.location.hash.search(current_modal_window.modal_window_name) === -1
             && current_modal_window.status) {
 
-            document.querySelector(".show_modal_window").classList
-                .toggle('show_modal_window')
-            current_modal_window.change_status()
+            let m_window = document.querySelector(".show_modal_window")
+            if (!m_window) return
+
+            current_modal_window.change_status(
+                '#' + m_window.getAttribute('id'))
         }
     })
 
@@ -69,4 +85,29 @@
         }
     })
 
+    function modal_window_toggle(event) {
+
+        let modal_window_btn=event.target.closest('[data-modal-window-toggle]');
+
+        if(!modal_window_btn) return;
+
+        event.preventDefault();
+        let modal_window_target=modal_window_btn.getAttribute('data-modal-window-toggle');
+
+        current_modal_window.change_status(modal_window_target)
+
+        // если модальное окно активно изменяем url, иначе откатываем
+        if (current_modal_window.status)
+            window.location.hash += current_modal_window.modal_window_name
+        else
+            history.back()
+    }
+
+    function modal_windows_position() {
+        let modal_windows = document.getElementsByClassName('modal')
+
+        for (let modal_window of modal_windows) {
+            modal_window.firstElementChild.style.left = -modal_window.offsetWidth + 'px'
+        }
+    }
 })()
