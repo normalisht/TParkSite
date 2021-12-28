796f480771edc76f79ea7b06c14e1949b9410461
SerenokMA <90979544+SerenokMA@users.noreply.github.com>
2021-12-28 06:31:41 +0300

    .

--- app/admin_panel/routes.py
+++ app/admin_panel/routes.py
@@ -40,10 +40,13 @@ def logout():
 @bp.route('/admin_panel/about', methods=['GET'])
 @login_required
 def main():
-    main_text = Text.query.filter_by(title="main_text").first().text
-    events = Event.query.all()
+    employees = Employee.query.all()
+    about = Text.query.filter_by(title='about').first()
+    filosofi = Text.query.filter_by(title='filosofi').first()
+    partners = Partner.query.all()
 
-    return render_template('аdmin_panel/about.html', title='Главная страница', main_text=main_text, events=events)
+    return render_template('аdmin_panel/about.html', title='О НАС', employees=employees,
+                           filosofi=filosofi, about=about, partners=partners)
 
 @bp.route('/menu', methods=['GET'])
 @login_required
