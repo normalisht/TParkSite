0c5d71ba4d6aa46e6471653d39c5d195f643d981
Noylifer <Sobko.2002@mail.ru>
2022-05-02 20:51:18 +0300

    .

--- app/static/gallery/gallery.js
+++ app/static/gallery/gallery.js
@@ -4,7 +4,7 @@ $(function(){
         $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'">');
         $('#magnify').css({
             left: ($(document).width() - $('#magnify').outerWidth())/2,
-            top: ($(window).height() - $('#magnify').outerHeight())/2
+            top: ($('#magnify').outerHeight())*2
         });
         $('#overlay, #magnify').fadeIn('fast');
     });
