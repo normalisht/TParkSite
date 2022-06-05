fd1656719006a6f2540d268fc12088193ef3bd9b
Losharik17 <dimanormanev@yandex.ru>
2022-06-05 19:27:59 +0300

    .

--- app/static/service/service.js
+++ app/static/service/service.js
@@ -1,16 +1,6 @@
-style_css()
-
-function style_css() {
-    let detect = new MobileDetect(window.navigator.userAgent)
-    let link = document.createElement('link')
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.media = 'all';
-
-    if (detect.mobile()) {
-        link.href = '/app/static/admin_panel_phone.css';
-    } else {
-        link.href = '/app/static/admin_panel.css'
-    }
-    document.head.append(link)
-}
+document.addEventListener('DOMContentLoaded', function () {
+    $('#phone_numbers').css('margin', '10px 0 -8px')
+        .find('a').each(function (index, element) {
+        $(element).css('margin', '15px auto 12px')
+    })
+})
