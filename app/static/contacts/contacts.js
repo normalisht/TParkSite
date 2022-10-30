9cf5bff56a0fad2dc9b61d85d32486fff7ae78dd
TorinLight <tujh.sad@gmail.com>
2022-10-31 00:54:16 +0300

    Закончил
    Стили работают корректно. Правки кончились. Продакшн.

--- app/static/contacts/contacts.js
+++ app/static/contacts/contacts.js
@@ -32,3 +32,21 @@ function style_css_dop() {
     document.getElementById('style').append(link)
 }
 
+style_css_desctop()
+
+function style_css_desctop() {
+    let detect = new MobileDetect(window.navigator.userAgent)
+    let link = document.createElement('link')
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.media = 'all';
+
+    if (detect.mobile()) {
+        link.href = '';
+    } else {
+        link.href = '/app/static/main/desktop.css';
+    }
+    document.getElementById('style').append(link)
+}
+
+
