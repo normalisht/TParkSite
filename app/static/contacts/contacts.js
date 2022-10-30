84d8a1ff0e2eea8b41b470b4e76f0844240c6340
TorinLight <tujh.sad@gmail.com>
2022-10-30 21:55:58 +0300

    /

--- app/static/contacts/contacts.js
+++ app/static/contacts/contacts.js
@@ -10,7 +10,24 @@ function style_css() {
     if (detect.mobile()) {
         link.href = '/app/static/contacts/contacts_phone.css';
     } else {
-        link.href = '/app/static/contacts/contacts_desktop.css'
+        link.href = '/app/static/contacts/contacts.css'
+    }
+    document.getElementById('style').append(link)
+}
+
+style_css_dop()
+
+function style_css_dop() {
+    let detect = new MobileDetect(window.navigator.userAgent)
+    let link = document.createElement('link')
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.media = 'all';
+
+    if (detect.mobile()) {
+        link.href = '/app/static/comments/comments_phone.css';
+    } else {
+        link.href = '/app/static/comments/comments.css';
     }
     document.getElementById('style').append(link)
 }
